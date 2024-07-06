eb241391aab0943acc56d7c56dcdbaba5199ddfc
Wozzy1 <134657893+Wozzy1@users.noreply.github.com>
2024-07-06 02:28:42 -0500

    Added Entity and Player

---
+++ Content/Entity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NinjaGame.Content
+{
+    public abstract class Entity
+    {
+        public enum Direction {
+            NORTH,
+            EAST,
+            SOUTH,
+            WEST
+        }
+
+        public string name;
+        public int hp;
+        public int atk;
+        public Vector2 position;
+        public Texture2D texture;
+
+        public Direction direction = Direction.SOUTH;
+
+        /**
+         * This rectangle is the size of the texture.
+         */ 
+        public virtual Rectangle Rect
+        {
+            get
+            {
+                return new Rectangle((int)position.X, (int)position.Y, 100, 100);
+            }
+        }
+
+        public Entity(string name, int hp, int atk, Vector2 position, Texture2D texture)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.atk = atk;
+            this.position = position;
+            this.texture = texture;
+        }
+
+        public Entity(Vector2 position, Texture2D texture)
+        {
+            //this.name = name;
+            //this.hp = hp;
+            //this.atk = atk;
+            this.position = position;
+            this.texture = texture;
+        }
+
+
+        public abstract bool Attack(Entity e);
+        public abstract void Update(GameTime gameTime);
+
+        public bool IsDead()
+        {
+            return hp <= 0;
+        }
+
+    }
+}
---
+++ Content/Player.cs
@@ -0,0 +1,87 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using NinjaGame.Content;
+
+namespace NinjaGame
+{
+    public class Player : Entity
+    {
+
+        public Direction facing = Direction.SOUTH;
+
+
+        public Player(string name, int hp, int atk, Vector2 position, Texture2D texture)
+            : base(name, hp, atk, position, texture)
+        {
+
+        }
+
+        /**
+         * Attacks an entity e dealing atk damage to e.
+         * 
+         * For now, returns true if the entity's hp is <= 0
+         */
+        public override bool Attack(Entity e)
+        {
+            
+            return false;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (IsDead())
+            {
+                // do something
+            }
+
+            float dX = 0;
+            float dY = 0;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                dX += 4;
+                facing = Direction.EAST;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                dX -= 4;
+                facing = Direction.WEST;
+            }
+
+            position.X += dX;
+
+            //foreach (var character in collisionGroup)
+            //{
+            //    if (character.Rect.Intersects(Rect))
+            //    {
+            //        position.X -= dX;
+            //    }
+            //}
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                dY -= 4;
+                facing = Direction.NORTH;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S) || Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                dY += 4;
+                facing = Direction.SOUTH;
+            }
+
+            position.Y += dY;
+
+            //foreach (var character in collisionGroup)
+            //{
+            //    if (character.Rect.Intersects(Rect))
+            //    {
+            //        position.Y -= dY;
+            //    }
+            //}
+
+        }
+    }
+
+}
